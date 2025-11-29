067ff22eb29e720a70f75f8e4ff09029c59c297a
Resham Rout <reshamrout@gmail.com>
2025-11-30 00:46:56 +0530

    feat: Complete Signup UI

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,115 +1,149 @@
-import React, {useState} from 'react'
-
+import React, { useState } from "react";
+import signuplogo from "../assets/signup.svg";
+import { useNavigate } from "react-router-dom";
 
 const SignupPage = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    cnfPassword: "",
+    accountType: "",
+  });
 
-    const [formData, setFormData] = useState({
-      firstName : "",
-      lastName : "",
-      email : "",
-      password : "",
-      cnfPassword : "",
-      accountType : "",
-    });
+  const { firstName, lastName, email, password, cnfPassword, accountType } = formData;
 
-    const {firstName, lastName, email, password, cnfPassword, accountType} = formData;
+  const navigate = useNavigate();
 
-    const handleChange = (e) =>{
-      setFormData((prevData)=>({
-        ...prevData,
-        [e.target.name] : e.target.value
-      }))
-    }
+  const handleChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   return (
-    <div>
+    <div className="w-screen h-screen bg-[#E8EFFF] flex justify-center items-center">
       <form>
-        <label>
-          <p>First Name</p>
-            <input
-              type='text'
-              name='firstName'
-              value = {firstName}
-              onChange={handleChange}
-              placeholder='Enter First Name'
-            >
-            </input>
-        </label>
+        <div className="bg-white min-w-md  overflow-hidden rounded-3xl  border-gray-400/40 border shadow-lg">
+          <div className="mx-5 mt-7 flex flex-col gap-2">
+            <div className="flex flex-col gap-2 justify-center items-center mt-8 mb-5">
+              <img src={signuplogo} className="w-20 h-20 object-cover"></img>
+              <p className="font-semibold">Create Account</p>
+              <p className="text-[#717182] ">
+                Sign up to start managing your tasks
+              </p>
+            </div>
 
-        <label>
-          <p>Last Name</p>
-            <input
-              type='text'
-              name='lastName'
-              value = {lastName}
-              onChange={handleChange}
-              placeholder='Enter Last Name'
-            >
-            </input>
-        </label>
+            <div className="w-full ">
+              <p className="text-center"></p>
+              <div className="flex bg-gray-200 rounded-full overflow-hidden w-52 mx-auto">
+                <button
+                  type="button"
+                  className={`flex-1 py-2 text-center transition-all duration-500 ${
+                    accountType === "User"
+                      ? "bg-indigo-600 flex-2 text-white"
+                      : "text-gray-700"
+                  }`}
+                  onClick={() =>
+                    setFormData({ ...formData, accountType: "User" })
+                  }
+                >
+                  User
+                </button>
+                <button
+                  type="button"
+                  onClick={() =>
+                    setFormData({ ...formData, accountType: "Admin" })
+                  }
+                  className={`flex-1 py-2 text-center transition-all duration-500 ${
+                    accountType === "Admin"
+                      ? "bg-indigo-600 flex-2 text-white"
+                      : "text-gray-700"
+                  }`}
+                >
+                  Admin
+                </button>
+              </div>
+            </div>
 
-        <label>
-          <p>Email</p>
-            <input
-              type='text'
-              name='email'
-              value = {email}
-              onChange={handleChange}
-              placeholder='Enter Email'
-            >
-            </input>
-        </label>
+            <div className="flex gap-4 mt-5 ">
+              <label className="">
+              <p>First Name</p>
+              <input
+                className="bg-[#F3F3F5] p-2 w-full rounded-lg border-transparent outline-none border-2 focus:border-gray-400"
+                type="text"
+                name="firstName"
+                value={firstName}
+                onChange={handleChange}
+                placeholder="Enter First Name"
+              ></input>
+            </label>
 
-        <label>
-          <p>Password</p>
-            <input
-              type='password'
-              name='password'
-              value = {password}
-              onChange={handleChange}
-              placeholder='Enter Password'
-            >
-            </input>
-        </label>
+            <label>
+              <p>Last Name</p>
+              <input
+                className="bg-[#F3F3F5] p-2 w-full rounded-lg border-transparent outline-none border-2 focus:border-gray-400"
+                type="text"
+                name="lastName"
+                value={lastName}
+                onChange={handleChange}
+                placeholder="Enter Last Name"
+              ></input>
+            </label>
+            </div>
 
-        <label>
-          <p>Confirm Password</p>
-            <input
-              type='text'
-              name='cnfPassword'
-              value = {cnfPassword}
-              onChange={handleChange}
-              placeholder='Enter Confirm Password'
-            >
-            </input>
-        </label>
-        <div>
-          <p>Account Type:</p>
-          <label>User
-            <input
-              type='radio'
-              name='accountType'
-              value="User"
-              checked={accountType === "User"}
-              onChange={handleChange}
-            >
-            </input>
-          </label>
+            <label>
+              <p>Email</p>
+              <input
+                className="bg-[#F3F3F5] p-2 w-full rounded-lg border-transparent outline-none border-2 focus:border-gray-400"
+                type="text"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                placeholder="Enter Email"
+              ></input>
+            </label>
 
-           <label>Admin
-            <input
-              type='radio'
-              name='accountType'
-              value="Admin"
-              checked={accountType === "Admin"}
-              onChange={handleChange}
-            >
-            </input>
-          </label>
+            <label>
+              <p>Password</p>
+              <input
+                className="bg-[#F3F3F5] p-2 w-full rounded-lg border-transparent outline-none border-2 focus:border-gray-400"
+                type="password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                placeholder="Enter Password"
+              ></input>
+            </label>
+
+            <label>
+              <p>Confirm Password</p>
+              <input
+                className="bg-[#F3F3F5] p-2 w-full rounded-lg border-transparent outline-none border-2 focus:border-gray-400"
+                type="text"
+                name="cnfPassword"
+                value={cnfPassword}
+                onChange={handleChange}
+                placeholder="Enter Confirm Password"
+              ></input>
+            </label>
+          </div>
+          <div className='mx-5 my-1'>
+                <button type='submit'
+                className='bg-black w-full text-white px-4 py-2 rounded-lg my-3 cursor-pointer '
+                >Create Account</button>
+                <p className='text-center mb-5 text-[#717182]'>Already have an account?? 
+                    <span className='text-blue-400 hover:underline cursor-pointer px-2'
+                    onClick={()=>navigate("/login")}
+                    >Sign in</span>
+                </p>
+            </div>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignupPage
+export default SignupPage;
