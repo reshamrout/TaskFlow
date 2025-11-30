a620f63299a04aee91b242811f162baf5028a0b3
Resham Rout <reshamrout@gmail.com>
2025-12-01 00:23:38 +0530

    feat(signup): completed signup api call

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,21 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import signuplogo from "../assets/signup.svg";
 import { useNavigate } from "react-router-dom";
+import api from "../services/api";
+import {toast} from 'react-hot-toast';
 
 const SignupPage = () => {
   const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
+    firstname: "",
+    lastname: "",
     email: "",
     password: "",
     cnfPassword: "",
     accountType: "",
   });
 
-  const { firstName, lastName, email, password, cnfPassword, accountType } = formData;
+  const { firstname, lastname, email, password, cnfPassword, accountType } = formData;
 
   const navigate = useNavigate();
-
   const handleChange = (e) => {
     setFormData((prevData) => ({
       ...prevData,
@@ -23,9 +24,27 @@ const SignupPage = () => {
     }));
   };
 
+  const handleSubmit = async (e) =>{
+    try{
+      e.preventDefault();
+      const response = await api.post("/auth/signup", {firstname, lastname, email, password, cnfPassword, accountType});
+      toast.success("User Registered Successfully, Please Login!");
+      navigate("/login");
+    }
+    catch(error){
+      if(error.response && error.response.data.message){
+        toast.error(error.response.data.message);
+      }
+      else{
+        console.log(error);
+         toast.error("Something went wrong. Try again!");
+      }
+    }
+  }
+
   return (
     <div className="w-screen h-screen bg-[#E8EFFF] flex justify-center items-center">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="bg-white min-w-md  overflow-hidden rounded-3xl  border-gray-400/40 border shadow-lg">
           <div className="mx-5 mt-7 flex flex-col gap-2">
             <div className="flex flex-col gap-2 justify-center items-center mt-8 mb-5">
@@ -74,8 +93,8 @@ const SignupPage = () => {
               <input
                 className="bg-[#F3F3F5] p-2 w-full rounded-lg border-transparent outline-none border-2 focus:border-gray-400"
                 type="text"
-                name="firstName"
-                value={firstName}
+                name="firstname"
+                value={firstname}
                 onChange={handleChange}
                 placeholder="Enter First Name"
               ></input>
@@ -86,8 +105,8 @@ const SignupPage = () => {
               <input
                 className="bg-[#F3F3F5] p-2 w-full rounded-lg border-transparent outline-none border-2 focus:border-gray-400"
                 type="text"
-                name="lastName"
-                value={lastName}
+                name="lastname"
+                value={lastname}
                 onChange={handleChange}
                 placeholder="Enter Last Name"
               ></input>
@@ -122,7 +141,7 @@ const SignupPage = () => {
               <p>Confirm Password</p>
               <input
                 className="bg-[#F3F3F5] p-2 w-full rounded-lg border-transparent outline-none border-2 focus:border-gray-400"
-                type="text"
+                type="password"
                 name="cnfPassword"
                 value={cnfPassword}
                 onChange={handleChange}
@@ -131,7 +150,8 @@ const SignupPage = () => {
             </label>
           </div>
           <div className='mx-5 my-1'>
-                <button type='submit'
+                <button 
+                type='submit'
                 className='bg-black w-full text-white px-4 py-2 rounded-lg my-3 cursor-pointer '
                 >Create Account</button>
                 <p className='text-center mb-5 text-[#717182]'>Already have an account?? 
