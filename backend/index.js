9d05994fffa72663aa29be4ccc5bce51563471b0
Resham Rout <reshamrout@gmail.com>
2025-12-02 23:06:23 +0530

    fix: Fixed cookies issue

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,10 @@ const User = require('./models/User');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 const PORT = process.env.PORT || 4000
