a0c1b79e5e98abf86426c79ee3301468f98ee2aa
Resham Rout <reshamrout@gmail.com>
2025-12-04 00:21:08 +0530

    fix: fixed wrong success response for delete task

--- backend/controllers/Task.js
+++ backend/controllers/Task.js
@@ -173,7 +173,7 @@ exports.deleteTask = async(req, res) => {
 
         await Task.findByIdAndDelete(taskId);
         return res.status(200).json({
-            success: false,
+            success: true,
             message: "Task Deleted Successfully",
         })
     }
